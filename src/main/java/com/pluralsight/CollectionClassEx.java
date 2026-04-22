ce91a1b95cd49fdf1077db7664d0745434153eca
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-22 13:42:07 -0500

    add array list

---
+++ src/main/java/com/pluralsight/CollectionClassEx.java
@@ -0,0 +1,38 @@
+package com.pluralsight;
+import java.util.ArrayList;
+//array list example
+
+public class CollectionClassEx {
+    public static void main(String[] args) {
+        // resizable array
+        // T = generic type give away
+
+        //create the empty list
+        ArrayList<String> kids = new ArrayList<>();
+
+        //add 3 rugrats to the list
+        kids.add("bob");
+        kids.add("sally");
+        kids.add("timmy");
+
+        //print out the list
+        System.out.println(kids);
+
+
+        // loop over the list awith a traditional for loop
+        for(int i = 0; 1 < kids.size(); i++){
+            // use get() to get it by the index
+            System.out.println(kids.get(i) );
+        }
+        // use the for-each
+        int i = 1;
+        for (String kid : kids){
+            System.out.println(i + "." + kid);
+            i++;
+
+
+            // .size is the length of the list, return number of
+        }
+
+    }
+}
